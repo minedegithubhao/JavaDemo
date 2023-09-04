3708f56001f8dcfae9914ea4d0321d8885af1b6a
minedegithubhao <824737880@qq.com>
2023-09-04 21:57:20 +0800

    commit

--- spring-di-03/src/test/java/MyTest.java
+++ spring-di-03/src/test/java/MyTest.java
@@ -1,5 +1,7 @@
 import org.example.service.AccountService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -7,6 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @date 2023-09-03 22:13
  */
 public class MyTest {
+
+//    @Autowired
+//    private ApplicationContext applicationContext;
 
     @Test
     public void test(){
@@ -19,5 +24,8 @@ public class MyTest {
         System.out.println("after money transfer");
         System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
         System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+//        AccountService accountServiceImpl = applicationContext.getBean("accountServiceImpl", AccountService.class);
+//        System.out.println(accountServiceImpl.getAccount(1));
     }
 }
