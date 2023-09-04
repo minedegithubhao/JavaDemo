0914662b04ea488e90bc9634f41aff2346ad2cb0
minedegithubhao <824737880@qq.com>
2023-09-04 22:22:22 +0800

    s

---
+++ spring-Profiles/src/main/java/org/example/dao/UseMapper.java
@@ -0,0 +1,6 @@
+package org.example.dao;
+
+public interface UseMapper {
+
+    String selectUserName(String id);
+}
---
+++ spring-Profiles/src/main/java/org/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.service.impl;
+
+import org.example.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public String findUserName(String id) {
+        return null;
+    }
+}
